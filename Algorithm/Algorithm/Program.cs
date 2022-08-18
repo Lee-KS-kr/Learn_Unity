3b21b39d8b772bec61ad063762e007814152c30c
Kyungseo Lee <kyungseolee0815@gmail.com>
2022-08-19 00:06:29 +0900

    미로 랜덤 생성, 플레이어 이동(우수법)

--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -6,10 +6,14 @@ namespace Algorithm
     {
         public static void Main(string[] args)
         {
+            Board board = new Board();
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
             Console.CursorVisible = false;
 
             const int WAIT_TICK = 1000 / 30;
-            const char CIRCLE = '\u25cf';
 
             int lastTick = 0;
             
@@ -22,23 +26,18 @@ namespace Algorithm
                 // 만약에 경과한 시간이 1/30초보다 작다면
                 if (currentTick - lastTick < WAIT_TICK)
                     continue;
+                int deltaTick = currentTick - lastTick;
                 lastTick = currentTick;
                 #endregion
-                
+
                 // 입력 : 사용자의 모든 인풋을 감지하는 단계
+
                 // 로직 : 그 입력을 따라 게임 로직이 실행된다
+                player.Update(deltaTick);
+
                 // 렌더링 : 연산된 게임 세상을 그려준다
                 Console.SetCursorPosition(0, 0);
-                for (int i = 0; i < 25; i++)
-                {
-                    for (int j = 0; j < 25; j++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(CIRCLE);
-                    }
-
-                    Console.WriteLine();
-                }
+                board.Render();
             }
         }
     }
