9e7b7c9893d3265209e6d710d58d1451f8aa893e
Kyungseo Lee <kyungseolee0815@gmail.com>
2022-08-25 16:16:08 +0900

    에셋

---
+++ MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    private static Managers instance; // 유일성이 보장된다
+
+    public static Managers Instance
+    {
+        get
+        {
+            Init();
+            return instance;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if (instance == null)
+        {
+            // 초기화
+            GameObject obj = GameObject.Find("@Managers");
+            if (obj == null)
+            {
+                obj = new GameObject {name = "@Managers"};
+                obj.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(obj);
+            instance = obj.GetComponent<Managers>();
+        }
+    }
+}
