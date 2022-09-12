9ab06d4b91e752fd86beb0e8afb84dd1dcae7e01
Kyungseo Lee <kyungseolee0815@gmail.com>
2022-09-13 00:18:19 +0900

    코루틴

--- MMO_Unity/Assets/Scripts/Scenes/GameScene.cs
+++ MMO_Unity/Assets/Scripts/Scenes/GameScene.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    private Coroutine co;
+    
     protected override void Init()
     {
         base.Init();
         SceneType = Define.Scene.Game;
         Managers.UI.ShowSceneUI<UI_Inventory>();
-        
-        for (int i = 0; i < 5; i++)
-            Managers.ResourceManager.Instantiate("UnityChan");
+
+        co = StartCoroutine("ExplodeAfterSeconds", 4f);
+        StartCoroutine("StopExplode", 2f);
     }
 
     public override void Clear()
     {
         
+    }
+
+    IEnumerator ExplodeAfterSeconds(float seconds)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Exploded");
+        co = null;
+    }
+
+    IEnumerator StopExplode(float seconds)
+    {
+        Debug.Log("Stop Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Stop Execute");
+        if (co != null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
     }
 }
